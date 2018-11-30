6a884b83e5635176b59529fe3f6008fe67839248
Melvin Werthmüller <melvin@ideas4you.ch>
2018-11-30 11:00:10 +0100

    translate cubes instead of multiple instances... cause was not needed :P

--- webgl/3DTicTacToe.js
+++ webgl/3DTicTacToe.js
@@ -40,188 +40,8 @@ var player2 = {
 };
 
 
-// t = top
-// m = middle
-// g = ground
-// f = front
-// b = back
-// l = left
-// r = right
+
 var vertices = {
-    ftl: [
-        // back
-        0.5,-1.5,0.5, //v0
-        0.5,-0.5,0.5,  //v1
-        0.5,-0.5,1.5,   //v2
-        0.5,-1.5,1.5,  //v3
-
-        //mront
-        1.5,-1.5,0.5,  //v4
-        1.5,-0.5,0.5,   //v5
-        1.5,-0.5,1.5,    //v6
-        1.5,-1.5,1.5    //v7
-    ],
-    ftm: [
-        // back
-        0.5,-0.5,0.5, //v0
-        0.5,0.5,0.5,  //v1
-        0.5,0.5,1.5,   //v2
-        0.5,-0.5,1.5,  //v3
-
-        //mront
-        1.5,-0.5,0.5,  //v4
-        1.5,0.5,0.5,   //v5
-        1.5,0.5,1.5,    //v6
-        1.5,-0.5,1.5    //v7
-    ],
-    ftr: [
-        // back
-        0.5,0.5,0.5, //v0
-        0.5,1.5,0.5,  //v1
-        0.5,1.5,1.5,   //v2
-        0.5,0.5,1.5,  //v3
-
-        //mront
-        1.5,0.5,0.5,  //v4
-        1.5,1.5,0.5,   //v5
-        1.5,1.5,1.5,    //v6
-        1.5,0.5,1.5    //v7
-    ],
-    fml: [
-        // back
-        0.5,-1.5,-0.5, //v0
-        0.5,-0.5,-0.5,  //v1
-        0.5,-0.5,0.5,   //v2
-        0.5,-1.5,0.5,  //v3
-
-        //mront
-        1.5,-1.5,-0.5,  //v4
-        1.5,-0.5,-0.5,   //v5
-        1.5,-0.5,0.5,    //v6
-        1.5,-1.5,0.5    //v7
-    ],
-    fmm: [
-        // back
-        0.5,-0.5,-0.5, //v0
-        0.5,0.5,-0.5,  //v1
-        0.5,0.5,0.5,   //v2
-        0.5,-0.5,0.5,  //v3
-
-        //mront
-        1.5,-0.5,-0.5,  //v4
-        1.5,0.5,-0.5,   //v5
-        1.5,0.5,0.5,    //v6
-        1.5,-0.5,0.5    //v7
-    ],
-    fmr: [
-        // back
-        0.5,0.5,-0.5, //v0
-        0.5,1.5,-0.5,  //v1
-        0.5,1.5,0.5,   //v2
-        0.5,0.5,0.5,  //v3
-
-        //mront
-        1.5,0.5,-0.5,  //v4
-        1.5,1.5,-0.5,   //v5
-        1.5,1.5,0.5,    //v6
-        1.5,0.5,0.5    //v7
-    ],
-    fgl: [
-        // back
-        0.5,-1.5,-1.5, //v0
-        0.5,-0.5,-1.5,  //v1
-        0.5,-0.5,-0.5,   //v2
-        0.5,-1.5,-0.5,  //v3
-
-        //mront
-        1.5,-1.5,-1.5,  //v4
-        1.5,-0.5,-1.5,   //v5
-        1.5,-0.5,-0.5,    //v6
-        1.5,-1.5,-0.5    //v7
-    ],
-    fgm: [
-        // back
-        0.5,-0.5,-1.5, //v0
-        0.5,0.5,-1.5,  //v1
-        0.5,0.5,-0.5,   //v2
-        0.5,-0.5,-0.5,  //v3
-
-        //mront
-        1.5,-0.5,-1.5,  //v4
-        1.5,0.5,-1.5,   //v5
-        1.5,0.5,-0.5,    //v6
-        1.5,-0.5,-0.5    //v7
-    ],
-    fgr: [
-        // back
-        0.5,0.5,-1.5, //v0
-        0.5,1.5,-1.5,  //v1
-        0.5,1.5,-0.5,   //v2
-        0.5,0.5,-0.5,  //v3
-
-        //mront
-        1.5,0.5,-1.5,  //v4
-        1.5,1.5,-1.5,   //v5
-        1.5,1.5,-0.5,    //v6
-        1.5,0.5,-0.5    //v7
-    ],
-
-
-
-
-
-    mtl: [
-        // back
-        -0.5,-1.5,0.5, //v0
-        -0.5,-0.5,0.5,  //v1
-        -0.5,-0.5,1.5,   //v2
-        -0.5,-1.5,1.5,  //v3
-
-        //mront
-        0.5,-1.5,0.5,  //v4
-        0.5,-0.5,0.5,   //v5
-        0.5,-0.5,1.5,    //v6
-        0.5,-1.5,1.5    //v7
-    ],
-    mtm: [
-        // back
-        -0.5,-0.5,0.5, //v0
-        -0.5,0.5,0.5,  //v1
-        -0.5,0.5,1.5,   //v2
-        -0.5,-0.5,1.5,  //v3
-
-        //mront
-        0.5,-0.5,0.5,  //v4
-        0.5,0.5,0.5,   //v5
-        0.5,0.5,1.5,    //v6
-        0.5,-0.5,1.5    //v7
-    ],
-    mtr: [
-        // back
-        -0.5,0.5,0.5, //v0
-        -0.5,1.5,0.5,  //v1
-        -0.5,1.5,1.5,   //v2
-        -0.5,0.5,1.5,  //v3
-
-        //mront
-        0.5,0.5,0.5,  //v4
-        0.5,1.5,0.5,   //v5
-        0.5,1.5,1.5,    //v6
-        0.5,0.5,1.5    //v7
-    ],
-    mml: [
-        // back
-        -0.5,-1.5,-0.5, //v0
-        -0.5,-0.5,-0.5,  //v1
-        -0.5,-0.5,0.5,   //v2
-        -0.5,-1.5,0.5,  //v3
-
-        //mront
-        0.5,-1.5,-0.5,  //v4
-        0.5,-0.5,-0.5,   //v5
-        0.5,-0.5,0.5,    //v6
-        0.5,-1.5,0.5    //v7
-    ],
     mmm: [
         // back
         -0.5,-0.5,-0.5, //v0
@@ -234,179 +54,6 @@ var vertices = {
         0.5,0.5,-0.5,   //v5
         0.5,0.5,0.5,    //v6
         0.5,-0.5,0.5    //v7
-    ],
-    mmr: [
-        // back
-        -0.5,0.5,-0.5, //v0
-        -0.5,1.5,-0.5,  //v1
-        -0.5,1.5,0.5,   //v2
-        -0.5,0.5,0.5,  //v3
-
-        //mront
-        0.5,0.5,-0.5,  //v4
-        0.5,1.5,-0.5,   //v5
-        0.5,1.5,0.5,    //v6
-        0.5,0.5,0.5    //v7
-    ],
-    mgl: [
-        // back
-        -0.5,-1.5,-1.5, //v0
-        -0.5,-0.5,-1.5,  //v1
-        -0.5,-0.5,-0.5,   //v2
-        -0.5,-1.5,-0.5,  //v3
-
-        //mront
-        0.5,-1.5,-1.5,  //v4
-        0.5,-0.5,-1.5,   //v5
-        0.5,-0.5,-0.5,    //v6
-        0.5,-1.5,-0.5    //v7
-    ],
-    mgm: [
-        // back
-        -0.5,-0.5,-1.5, //v0
-        -0.5,0.5,-1.5,  //v1
-        -0.5,0.5,-0.5,   //v2
-        -0.5,-0.5,-0.5,  //v3
-
-        //mront
-        0.5,-0.5,-1.5,  //v4
-        0.5,0.5,-1.5,   //v5
-        0.5,0.5,-0.5,    //v6
-        0.5,-0.5,-0.5    //v7
-    ],
-    mgr: [
-        // back
-        -0.5,0.5,-1.5, //v0
-        -0.5,1.5,-1.5,  //v1
-        -0.5,1.5,-0.5,   //v2
-        -0.5,0.5,-0.5,  //v3
-
-        //mront
-        0.5,0.5,-1.5,  //v4
-        0.5,1.5,-1.5,   //v5
-        0.5,1.5,-0.5,    //v6
-        0.5,0.5,-0.5    //v7
-    ],
-
-
-
-
-    btl: [
-        // back
-        -1.5,-1.5,0.5, //v0
-        -1.5,-0.5,0.5,  //v1
-        -1.5,-0.5,1.5,   //v2
-        -1.5,-1.5,1.5,  //v3
-
-        //mront
-        -0.5,-1.5,0.5,  //v4
-        -0.5,-0.5,0.5,   //v5
-        -0.5,-0.5,1.5,    //v6
-        -0.5,-1.5,1.5    //v7
-    ],
-    btm: [
-        // back
-        -1.5,-0.5,0.5, //v0
-        -1.5,0.5,0.5,  //v1
-        -1.5,0.5,1.5,   //v2
-        -1.5,-0.5,1.5,  //v3
-
-        //mront
-        -0.5,-0.5,0.5,  //v4
-        -0.5,0.5,0.5,   //v5
-        -0.5,0.5,1.5,    //v6
-        -0.5,-0.5,1.5    //v7
-    ],
-    btr: [
-        // back
-        -1.5,0.5,0.5, //v0
-        -1.5,1.5,0.5,  //v1
-        -1.5,1.5,1.5,   //v2
-        -1.5,0.5,1.5,  //v3
-
-        //mront
-        -0.5,0.5,0.5,  //v4
-        -0.5,1.5,0.5,   //v5
-        -0.5,1.5,1.5,    //v6
-        -0.5,0.5,1.5    //v7
-    ],
-    bml: [
-        // back
-        -1.5,-1.5,-0.5, //v0
-        -1.5,-0.5,-0.5,  //v1
-        -1.5,-0.5,0.5,   //v2
-        -1.5,-1.5,0.5,  //v3
-
-        //mront
-        -0.5,-1.5,-0.5,  //v4
-        -0.5,-0.5,-0.5,   //v5
-        -0.5,-0.5,0.5,    //v6
-        -0.5,-1.5,0.5    //v7
-    ],
-    bmm: [
-        // back
-        -1.5,-0.5,-0.5, //v0
-        -1.5,0.5,-0.5,  //v1
-        -1.5,0.5,0.5,   //v2
-        -1.5,-0.5,0.5,  //v3
-
-        //mront
-        -0.5,-0.5,-0.5,  //v4
-        -0.5,0.5,-0.5,   //v5
-        -0.5,0.5,0.5,    //v6
-        -0.5,-0.5,0.5    //v7
-    ],
-    bmr: [
-        // back
-        -1.5,0.5,-0.5, //v0
-        -1.5,1.5,-0.5,  //v1
-        -1.5,1.5,0.5,   //v2
-        -1.5,0.5,0.5,  //v3
-
-        //mront
-        -0.5,0.5,-0.5,  //v4
-        -0.5,1.5,-0.5,   //v5
-        -0.5,1.5,0.5,    //v6
-        -0.5,0.5,0.5    //v7
-    ],
-    bgl: [
-        // back
-        -1.5,-1.5,-1.5, //v0
-        -1.5,-0.5,-1.5,  //v1
-        -1.5,-0.5,-0.5,   //v2
-        -1.5,-1.5,-0.5,  //v3
-
-        //mront
-        -0.5,-1.5,-1.5,  //v4
-        -0.5,-0.5,-1.5,   //v5
-        -0.5,-0.5,-0.5,    //v6
-        -0.5,-1.5,-0.5    //v7
-    ],
-    bgm: [
-        // back
-        -1.5,-0.5,-1.5, //v0
-        -1.5,0.5,-1.5,  //v1
-        -1.5,0.5,-0.5,   //v2
-        -1.5,-0.5,-0.5,  //v3
-
-        //mront
-        -0.5,-0.5,-1.5,  //v4
-        -0.5,0.5,-1.5,   //v5
-        -0.5,0.5,-0.5,    //v6
-        -0.5,-0.5,-0.5    //v7
-    ],
-    bgr: [
-        // back
-        -1.5,0.5,-1.5, //v0
-        -1.5,1.5,-1.5,  //v1
-        -1.5,1.5,-0.5,   //v2
-        -1.5,0.5,-0.5,  //v3
-
-        //mront
-        -0.5,0.5,-1.5,  //v4
-        -0.5,1.5,-1.5,   //v5
-        -0.5,1.5,-0.5,    //v6
-        -0.5,0.5,-0.5    //v7
     ]
 };
 
@@ -500,37 +147,7 @@ function draw() {
 
 
     // definition front
-    var wiredCube_ftl = new WireFrameCube(gl, vertices.ftl);
-    var wiredCube_ftm = new WireFrameCube(gl, vertices.ftm);
-    var wiredCube_ftr = new WireFrameCube(gl, vertices.ftr);
-    var wiredCube_fml = new WireFrameCube(gl, vertices.fml);
-    var wiredCube_fmm = new WireFrameCube(gl, vertices.fmm);
-    var wiredCube_fmr = new WireFrameCube(gl, vertices.fmr);
-    var wiredCube_fgl = new WireFrameCube(gl, vertices.fgl);
-    var wiredCube_fgm = new WireFrameCube(gl, vertices.fgm);
-    var wiredCube_fgr = new WireFrameCube(gl, vertices.fgr);
-
-    // definition middle
-    var wiredCube_mtl = new WireFrameCube(gl, vertices.mtl);
-    var wiredCube_mtm = new WireFrameCube(gl, vertices.mtm);
-    var wiredCube_mtr = new WireFrameCube(gl, vertices.mtr);
-    var wiredCube_mml = new WireFrameCube(gl, vertices.mml);
     var wiredCube_mmm = new WireFrameCube(gl, vertices.mmm);
-    var wiredCube_mmr = new WireFrameCube(gl, vertices.mmr);
-    var wiredCube_mgl = new WireFrameCube(gl, vertices.mgl);
-    var wiredCube_mgm = new WireFrameCube(gl, vertices.mgm);
-    var wiredCube_mgr = new WireFrameCube(gl, vertices.mgr);
-
-    // definition back
-    var wiredCube_btl = new WireFrameCube(gl, vertices.btl);
-    var wiredCube_btm = new WireFrameCube(gl, vertices.btm);
-    var wiredCube_btr = new WireFrameCube(gl, vertices.btr);
-    var wiredCube_bml = new WireFrameCube(gl, vertices.bml);
-    var wiredCube_bmm = new WireFrameCube(gl, vertices.bmm);
-    var wiredCube_bmr = new WireFrameCube(gl, vertices.bmr);
-    var wiredCube_bgl = new WireFrameCube(gl, vertices.bgl);
-    var wiredCube_bgm = new WireFrameCube(gl, vertices.bgm);
-    var wiredCube_bgr = new WireFrameCube(gl, vertices.bgr);
 
     var solidSphere = new SolidSphere(gl, 60, 60);
 
@@ -539,7 +156,7 @@ function draw() {
     var identityMatrix = mat4.create();
     mat4.identity(identityMatrix);
     var loop = function() {
-        angel = performance.now() / 1000 / 6 * 2 * Math.PI;
+        //angel = performance.now() / 1000 / 6 * 2 * Math.PI;
 
         gl.clearColor(0.5, 0.5, 0.5, 1);
         gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
@@ -574,40 +191,107 @@ function draw() {
         // GRIDE
 
         mat4.rotate(modelMatrix, identityMatrix, angel, [0,0,1]);
+
+        var modelMatrixTmp = mat4.create();
+        mat4.copy(modelMatrixTmp, modelMatrix);
         gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrix);
 
-        // draw middle
-        wiredCube_ftl.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_ftm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_ftr.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_fml.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_fmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_fmr.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_fgl.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_fgm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_fgr.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-
-        // draw middle
-        wiredCube_mtl.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_mtm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_mtr.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_mml.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+        // draw middle cube
         wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_mmr.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_mgl.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_mgm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_mgr.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
 
-        // draw middle
-        wiredCube_btl.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_btm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_btr.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_bml.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_bmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_bmr.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_bgl.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_bgm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
-        wiredCube_bgr.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+
+        // middle
+        mat4.translate(modelMatrixTmp, modelMatrix, [0,-1,1]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+        mat4.translate(modelMatrixTmp, modelMatrix, [0,0,1]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+        mat4.translate(modelMatrixTmp, modelMatrix, [0,1,1]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+
+        mat4.translate(modelMatrixTmp, modelMatrix, [0,-1,0]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+        mat4.translate(modelMatrixTmp, modelMatrix, [0,1,0]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+
+        mat4.translate(modelMatrixTmp, modelMatrix, [0,-1,-1]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+        mat4.translate(modelMatrixTmp, modelMatrix, [0,0,-1]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+        mat4.translate(modelMatrixTmp, modelMatrix, [0,1,-1]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+
+
+        // back
+        mat4.translate(modelMatrixTmp, modelMatrix, [-1,-1,1]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+        mat4.translate(modelMatrixTmp, modelMatrix, [-1,0,1]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+        mat4.translate(modelMatrixTmp, modelMatrix, [-1,1,1]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+
+        mat4.translate(modelMatrixTmp, modelMatrix, [-1,-1,0]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+        mat4.translate(modelMatrixTmp, modelMatrix, [-1,0,0]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+        mat4.translate(modelMatrixTmp, modelMatrix, [-1,1,0]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+
+        mat4.translate(modelMatrixTmp, modelMatrix, [-1,-1,-1]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+        mat4.translate(modelMatrixTmp, modelMatrix, [-1,0,-1]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+        mat4.translate(modelMatrixTmp, modelMatrix, [-1,1,-1]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+
+
+        // front
+        mat4.translate(modelMatrixTmp, modelMatrix, [1,-1,1]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+        mat4.translate(modelMatrixTmp, modelMatrix, [1,0,1]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+        mat4.translate(modelMatrixTmp, modelMatrix, [1,1,1]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+
+        mat4.translate(modelMatrixTmp, modelMatrix, [1,-1,0]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+        mat4.translate(modelMatrixTmp, modelMatrix, [1,-0,0]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+        mat4.translate(modelMatrixTmp, modelMatrix, [1,1,0]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+
+        mat4.translate(modelMatrixTmp, modelMatrix, [1,-1,-1]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+        mat4.translate(modelMatrixTmp, modelMatrix, [1,0,-1]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+        mat4.translate(modelMatrixTmp, modelMatrix, [1,1,-1]);
+        gl.uniformMatrix4fv(ctx.uModelMatrixId, false, modelMatrixTmp);
+        wiredCube_mmm.draw(gl, ctx.aVertexPositionId, ctx.aColorPositionId);
+
 
         requestAnimationFrame(loop);
     };
